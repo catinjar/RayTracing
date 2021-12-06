465de415ea996af80a8da98c5cb865cb596f1cb0
denis_vyatkin <catinjar42@gmail.com>
2021-12-06 20:01:25 +0500

    Commenting

--- src/ColorHelpers.cpp
+++ src/ColorHelpers.cpp
@@ -1,5 +1,7 @@
 #include "ColorHelpers.hpp"
 
+// Some samples might be NaN or a very big number and kill the whole pixel during the Monte-Carlo averaging
+// We hide this issue here
 Vector3 DeNan(const Vector3 &color)
 {
 	auto temp = color;
